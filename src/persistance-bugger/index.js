c1916b28dc94169733ae714ff385c5ba7cba4694
Iván Sánchez Díaz <ivansanchez@redradix.com>
2023-01-17 13:21:23 +0100

    fix: fixed double module exportation

--- src/persistance-bugger/index.js
+++ src/persistance-bugger/index.js
@@ -1,24 +1,25 @@
-const isOneDigit = (num) => num > 9
+const isOneDigit = (num) => num > 9;
 
-const splitNumber = (num) => num.toString().split('')
+const splitNumber = (num) => num.toString().split("");
 
-const reduceMultiplication = (previousValue, currentValue) => previousValue * currentValue
+const reduceMultiplication = (previousValue, currentValue) =>
+  previousValue * currentValue;
 
 const persistence = (num) => {
-  let counter = 0
+  let counter = 0;
 
   if (isOneDigit(num)) {
-    let splittedNumber = splitNumber(num)
+    let splittedNumber = splitNumber(num);
 
     while (splittedNumber.length > 1) {
-      counter++
-      splittedNumber = splitNumber(splittedNumber.reduce(reduceMultiplication, 1))
+      counter++;
+      splittedNumber = splitNumber(
+        splittedNumber.reduce(reduceMultiplication, 1)
+      );
     }
   }
 
-  return counter
-}
+  return counter;
+};
 
-module.exports = persistence
-
-module.exports = persistence
+module.exports = persistence;
